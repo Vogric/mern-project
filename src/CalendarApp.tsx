68b71f58b910af548d2827ed6bbf0c4bd938ae31
vogric <blvogric@gmail.com>
2021-01-24 22:02:15 -0300

    App Setup, TypeScript and react-router-dom were installed

---
+++ src/CalendarApp.tsx
@@ -0,0 +1,10 @@
+export const CalendarApp = () => {
+  return (
+    <div>
+      <h1>CalendarApp</h1>
+      <hr />
+    </div>
+  );
+};
+
+export default CalendarApp;
---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import ReactDOM from "react-dom";
+import CalendarApp from "./CalendarApp";
+
+ReactDOM.render(<CalendarApp />, document.getElementById("root"));
